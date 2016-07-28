cec41ec9da4bda2789003f218b4e55eaa52a1b70
pultitom <t.pultinevicius@gmail.com>
2016-07-28 22:28:11 +0300

    added customer list, extracted it to customer component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 
+import { CustomerComponent } from './customer/customer.component';
+
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/app.component.html',
+    directives: [CustomerComponent]
 })
 export class AppComponent { 
     title = 'My First Angular 2 App';
     headerColor = 'blue';
     name = 'Tomas';
+
+    customers = [
+        { id: 1, name: "Tomas" },
+        { id: 2, name: "John" },
+        { id: 3, name: "Arnold" },
+        { id: 4, name: "Matt" },
+        { id: 5, name: "Katrin" }
+    ];
+
 
     buttonClick() {
         var color = this.headerColor === 'blue' ? 'red' : 'blue';
